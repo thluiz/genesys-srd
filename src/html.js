7f1385588cbc5cf24a29b9fe26a742beaeae9e77
Thiago Luiz Silva <contato@thluiz.com>
2019-06-13 08:01:12 -0300

    Adicionando bootstrap e jquery como estaticos

--- src/html.js
+++ src/html.js
@@ -23,14 +23,13 @@ export default class HTML extends React.Component {
           {config.siteMetadata.favicon ?
             (<link rel="shortcut icon" type="image/svg" href={config.siteMetadata.favicon} />) : null
           }
-          <link rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
-          <script
-  			  src="https://code.jquery.com/jquery-3.3.1.min.js"
-  			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  			  crossOrigin="anonymous"></script>
-          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossOrigin="anonymous"></script>
+
+          <link rel="stylesheet" href={"/bootstrap-3.3.7.min.css"} />
+
+          <script src={"/jquery-3.3.1.min.js"}></script>
+
+          <script src={"/bootstrap-3.3.7.min.js"}></script>
+
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
